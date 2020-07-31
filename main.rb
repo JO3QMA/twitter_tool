6cf43f2475813332fbdca67d95ccfa60c8143812
JO3QMA <g.dark.matter@gmail.com>
2020-07-31 12:11:24 +0900

    同じIDのツイートを1件目のみしか反応しないようにs修正

--- main.rb
+++ main.rb
@@ -4,25 +4,34 @@ require 'yaml'
 require 'twitter'
 require 'pp'
 
-class Favotter
+# 全処理
+class TweetController
+  # 初期化
   def initialize
+    puts '初期化開始'
+    puts 'config.ymlをロード'
     yaml_file = YAML.load_file('./config.yml')
     api = yaml_file['api']
     @option = yaml_file['option']
+    puts 'Twitter認証開始'
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = api['API_Key']
       config.consumer_secret     = api['API_Secret_Key']
       config.access_token        = api['Access_Token']
       config.access_token_secret = api['Access_Token_Secret']
     end
+    puts '最終ツイートID読み込み'
     @last_tweet_file = './tweet_id.txt'
     if File.exist?(@last_tweet_file)
       @last_tweet = File.read(@last_tweet_file).to_i
+      puts "最終ツイートID: #{@last_tweet}"
     else
-      @last_tweet = nil
+      @last_tweet = 0
+      puts '最終ツイートIDが読み込めませんでした。IDを0に設定します。'
     end
-    p @last_tweet
+    @id_list = []
     @tweet_list = []
+    puts '初期化終了'
   end
 
   def search_tweet
@@ -34,11 +43,14 @@ class Favotter
     since_id = @last_tweet
     p since_id
     p '取得開始'
-    # since_idの挙動怪しすぎて指定しないほうがいい説まである
     result = @client.search(query, result_type: result_type, exclude: 'retweets', since_id: since_id, lang: lang, count: count).take(count)
-    result.each do |tweet|
-      @tweet_list.push tweet.id
+    result.reverse_each do |tweet|
+      unless @id_list.include?(tweet.user.id)
+        @tweet_list.push tweet.id
+        @id_list.push tweet.user.id
+      end
     end
+    pp @tweet_list.size
     p '終了'
   end
 
@@ -51,9 +63,7 @@ class Favotter
   end
 
   def write_last_id
-    unless @tweet_list.size == 0
-      File.write(@last_tweet_file, @tweet_list[0])
-    end
+    File.write(@last_tweet_file, @tweet_list.last) unless @tweet_list.size == 0
   end
 
   def main
@@ -62,5 +72,5 @@ class Favotter
     write_last_id
   end
 end
-test = Favotter.new
+test = TweetController.new
 test.main
