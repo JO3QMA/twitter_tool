3997356d83b828787fbd2b522a27c10a93de8150
JO3QMA <g.dark.matter@gmail.com>
2021-12-01 11:02:54 +0900

    git pushのテスト用

---
+++ twitter_auth.rb
@@ -0,0 +1,21 @@
+require 'twitter'
+require 'yaml'
+
+# TwitterClientクラス
+class TwiClient
+  def initialize
+    # 初期化
+    config_file = YAML.load_file('./config.yml')
+    api = config_file['api']
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = api['API_Key']
+      config.consumer_secret     = api['API_Secret_Key']
+      config.access_token        = api['Access_Token']
+      config.access_token_secret = api['Access_Token_Secret']
+    end
+  end
+
+  def client
+    @client
+  end
+end
